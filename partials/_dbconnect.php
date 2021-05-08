26814dbc567f3717dc3be45bd655d06e63d86a7d
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2021-05-08 17:35:22 +0530

    ...

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -3,7 +3,7 @@
 
         $servername = "localhost";
         $username = "root";
-        $password = "sa";
+        $password = "";
         $database = "payroll";
 
         $con = mysqli_connect($servername,$username,$password,$database);
